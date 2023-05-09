11753e14c4332f01887960276a1174c168adaf6f
GGos3 <janpll0421@gmail.com>
2023-05-09 09:40:58 +0900

    feat: Val, Var 자료형 사용

---
+++ src/main/kotlin/chap02/section2/ValVar.kt
@@ -0,0 +1,16 @@
+package chap02.section2
+
+fun main() {
+    val number1 = 10
+    val number2: Int = 20
+    var numberOfBooks = 30
+    val init: Int
+
+    var language = "English"
+    language = "Korean"
+
+    init = 10
+
+    println("number: $number1, number2: $number2, init: $init")
+    println("language: $language, Books: $numberOfBooks")
+}
